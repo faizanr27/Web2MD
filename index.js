f02ce985e4de25ae4fcddf43038e267901c72c23
Faizan Raza <faizanms025347@gmail.com>
2025-03-06 23:56:39 +0530

    Added node js clustering for concurrency

--- index.js
+++ index.js
@@ -3,31 +3,47 @@ import cors from "cors";
 import giveWebsiteInfo from "./utils/scraper.js";
 import 'dotenv/config';
 import os from "os";
+import cluster from "cluster";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-const numCPUs = os.cpus().length;
-console.log(numCPUs);
+if(cluster.isPrimary){
+    const numCPUs = os.cpus().length;
 
-app.use(express.json());
-app.use(cors());
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+      }
 
-// Process URL and store embeddings
-app.post("/process-url", async (req, res) => {
-    try {
-        const { url } = req.body;
-        if (!url) return res.status(400).json({ error: "URL is required" });
-        const result = await giveWebsiteInfo(url);
+      cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Starting a new worker...`);
+        cluster.fork();
+      });
+} else {
+        const app = express();
+        const PORT = process.env.PORT || 5000;
+        // const numCPUs = os.cpus().length;
+        // console.log(numCPUs);
 
-        console.log(result);
+        app.use(express.json());
+        app.use(cors());
 
-        res.json({ message: "scraped successfully", url, result });
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error", details: error.message });
-    }
+        // Process URL and store embeddings
+        app.post("/process-url", async (req, res) => {
+            try {
+                const { url } = req.body;
+                if (!url) return res.status(400).json({ error: "URL is required" });
+                const result = await giveWebsiteInfo(url);
+
+                console.log(result);
+
+                res.json({ message: "scraped successfully", url, result });
+            } catch (error) {
+                res.status(500).json({ error: "Internal Server Error", details: error.message });
+            }
+        });
+
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT} with process id ${process.pid}`);
 });
+}
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
