1e1e7546460b81c723f28f89a3d1d2937991a7c1
Faizan Raza <faizanms025347@gmail.com>
2025-03-18 04:18:58 +0530

    added theme toggler

---
+++ frontend/components/ui/ThemeToggle.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useTheme } from "@/context/ThemeContext";
+import { Sun, Moon } from "lucide-react";
+
+export default function ThemeToggle() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-1 rounded-md border border-zinc-600/40 dark:hover:bg-zinc-700"
+    >
+      {theme === "light" ? (
+        <Moon className="h-4 w-4" />
+      ) : (
+        <Sun className="h-4 w-4" />
+      )}
+    </button>
+  );
+}
