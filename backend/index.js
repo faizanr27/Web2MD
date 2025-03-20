5f4118d66e4532fdbf5ca51ae2b42b5bd55e6e23
Faizan Raza <faizanms025347@gmail.com>
2025-03-20 22:27:21 +0530

    add express rate limiter

--- backend/index.js
+++ backend/index.js
@@ -1,63 +1,74 @@
 import express from "express";
 import cors from "cors";
 import giveWebsiteInfo from "./utils/scraper.js";
-import 'dotenv/config';
-import os from "os";
-import cluster from "cluster";
+import "dotenv/config";
 import Crawler from "./utils/crawler.js";
+import rateLimit from "express-rate-limit";
 
-if(cluster.isPrimary){
-    const numCPUs = os.cpus().length;
-
-    for (let i = 0; i < numCPUs - 2; i++) {
-        cluster.fork();
-      }
-
-      cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died. Starting a new worker...`);
-        cluster.fork();
-      });
-} else {
-        const app = express();
-        const PORT = process.env.PORT || 5000;
-        // const numCPUs = os.cpus().length;
-        // console.log(numCPUs);
-
-        app.use(express.json());
-        app.use(cors());
-
-        // Process URL and store embeddings
-        app.post("/scrape", async (req, res) => {
-            try {
-                const { url } = req.body;
-                if (!url) return res.status(400).json({ error: "URL is required" });
-                const result = await giveWebsiteInfo(url);
-
-                console.log(result);
-
-                res.json({ message: "scraped successfully", url, result });
-            } catch (error) {
-                res.status(500).json({ error: "Internal Server Error", details: error.message });
-            }
-        });
-        app.post("/crawl", async (req, res) => {
-            try {
-                const { url } = req.body;
-                if (!url) return res.status(400).json({ error: "URL is required" });
-                const result = await Crawler(url)
-
-                console.log(result);
-
-                res.json({ message: "scraped successfully", url, result });
-            } catch (error) {
-                res.status(500).json({ error: "Internal Server Error", details: error.message });
-            }
-        });
-
-
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT} with process id ${process.pid}`);
+const app = express();
+const PORT = process.env.PORT || 5000;
+const scrapeLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 5, // Limit each IP to 5 requests per minute
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+      error: "Too many requests, please try again after a minute."
+  }
 });
-}
 
+const crawlLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 2,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+      error: "Too many requests, please try again after a minute."
+  }
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://rj7qd65n-3000.inc1.devtunnels.ms",
+    ],
+  })
+);
 
+// Process URL and store embeddings
+app.post("/scrape", scrapeLimiter, async (req, res) => {
+  try {
+    const { url } = req.body;
+    if (!url) return res.status(400).json({ error: "URL is required" });
+    const result = await giveWebsiteInfo(url);
+
+    // console.log(result);
+
+    res.json({ message: "scraped successfully", url, result });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+});
+app.post("/crawl", crawlLimiter, async (req, res) => {
+  try {
+    const { url } = req.body;
+    if (!url) return res.status(400).json({ error: "URL is required" });
+    const result = await Crawler(url);
+    // console.log(result)
+
+    // console.log(markdown);
+
+    res.json({ message: "scraped successfully", url, result });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
